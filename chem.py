3244f9059712ac517338d044be5569718eaa510c
Damilola <dbodun56@gmail.com>
2022-11-22 11:20:42 +0100

    Added requirement.txt

--- chem.py
+++ chem.py
@@ -11,8 +11,7 @@ import random
 import string
 import pandas as pd
 import csv
-
-
+from rdkit.Chem import rdMolDescriptors as Desc
 
 name = ''.join(random.choices(string.ascii_uppercase + string.ascii_uppercase, k = 4))
 st.markdown("<h1 style='text-align: center'>Molecular properties predictor</h1>", unsafe_allow_html=True)
@@ -25,31 +24,25 @@ def get2D(smiles):
 def getDescriptors(smiles):
     mol = Chem.MolFromSmiles(smiles)
     mw = Descriptors.MolWt(mol)
-    
+    tpsa = Desc.CalcTPSA(mol)
     acceptors = Descriptors.NumHAcceptors(mol)
     donors  = Descriptors.NumHDonors(mol)
     logp = Descriptors.MolLogP(mol)
-    return mw, acceptors, donors, logp
+    return mw, acceptors, tpsa,  donors, logp
 
 @st.cache
-def generateCsv(mw, acceptors, donor, logp):
+def generateCsv(mw, acceptors, donor, logp, tpsa):
     
     data = {
         "Molecular weight": mw,
         "acceptors": acceptors,
         "donor": donor,
-        "logP": logp
+        "logP": logp,
+        "tpsa": tpsa
     }
     df = pd.DataFrame(data, index=["1"])
     csv = df.to_csv(index=False).encode('utf-8')
     return csv
-
-
-
-    
-
-
-
 
 
 form =st.form("form")
@@ -59,7 +52,7 @@ submitted = form.form_submit_button("Submit")
 if submitted:
     filename = get2D(input)
     st.image("pictures/"+filename)
-    mw,acceptors, donors, logp = getDescriptors(input)
+    mw,acceptors, tpsa, donors, logp = getDescriptors(input)
 
     
     col1, col2 = st.columns(2)
@@ -68,7 +61,8 @@ if submitted:
     col3, col4 = st.columns(2)
     col3.write(f"Number of H Donors: {donors}")
     col3.write(f"LogP: {logp}")
-    csv = generateCsv(mw, acceptors, donors, logp)
+    col3.write(f"TPSA: {tpsa}")
+    csv = generateCsv(mw, acceptors, donors, logp, tpsa)
     st.download_button("Download", file_name=name+".csv", data=csv, mime='text/csv')
    
     
